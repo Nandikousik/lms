929cb0fd9e0f4a34f994e27bd363841b6aaef081
Deep <deepnarayan.ohs@gmail.com>
2022-10-31 19:36:59 +0530

    commit

---
+++ lms_frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="text-muted py-5 border-top mt-5">
+            <div className="container">
+                <p className="float-end mb-1"><a href="#">Back to top</a></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus hic voluptatibu</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- lms_frontend/src/components/Home.js
+++ lms_frontend/src/components/Home.js
@@ -1,80 +1,119 @@
+import {Link} from 'react-router-dom';
+
 function Home() {
   return (
     <div className="container mt-4">
       {/* Latest Courses */}
-      <h3 className="pb-1 mb-4">Latest Courses</h3>
+      <h3 className="pb-1 mb-4">Latest Courses <a href="#" class="float-end">See All</a></h3>
       <div className="row">
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <Link to="/detail/1"><img src="logo512.png" class="card-img-top" alt="..." /></Link>
+            <div className="card-body">
+              <h5 className="card-title"><Link to="/detail/1">Course title</Link></h5>
+            </div>
+          </div>
         </div>
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
       </div>
       {/* End Latest Courses */}
       {/* Popular Courses */}
-      <h3 className="pb-1 mb-4 mt-5">Popular Courses</h3>
+      <h3 className="pb-1 mb-4 mt-5">Popular Courses<a href="#" class="float-end">See All</a></h3>
       <div className="row">
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
         <div className="col-md-3">
-        <div className="card">
-  <a href="#"><img src="logo512.png" class="card-img-top" alt="..."/></a>
-  <div className="card-body">
-    <h5 className="card-title"><a href="#">Course title</a></h5>
-  </div>
-</div>
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
         </div>
       </div>
       {/* End Popular Courses */}
+      {/* Popular Teachers */}
+      <h3 className="pb-1 mb-4 mt-5">Popular Teachers<a href="#" class="float-end">See All</a></h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src="logo512.png" class="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Popular Teachers */}
     </div>
   );
 }
