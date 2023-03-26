ec736da3f7d0451fa2239c977b274b5f2f0a9984
Deep <deepnarayan.ohs@gmail.com>
2023-03-26 08:45:59 +0530

    commit

--- lms_frontend/src/components/Teacher/UserList.js
+++ lms_frontend/src/components/Teacher/UserList.js
@@ -9,13 +9,13 @@ function UserList(){
                 </aside>
                 <section className='col-md-9'>        
                     <div className='card'>
-                        <h5 className='card-header'>Recommended Courses</h5>
+                        <h5 className='card-header'>My User</h5>
                         <div className='card-body'>
                             <table className="table table-bordered">
                                 <thead>
                                     <tr>
                                         <th>Name</th>
-                                        <th>Created By</th>
+                                        <th>Enrolled Course</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
